1b7bc5518b7481c80cd7c03abbfebd87fb251d39
reeered <11667996+reeered@user.noreply.gitee.com>
2023-06-03 17:17:41 +0000

    update Test.py.
    Signed-off-by: reeered <11667996+reeered@user.noreply.gitee.com>

--- Test.py
+++ Test.py
@@ -9,10 +9,10 @@ class TestClass(unittest.TestCase):
     def setUp(self):
         self.game_map = Map([[1, 0, 1], [0, 1, 0], [0, 0, 0], [1, 1, 0]])
 
-    def test_rows(self):
+    def test_height(self):
         self.assertEqual(4, self.game_map.height, "Should get correct rows")
 
-    def test_cols(self):
+    def test_width(self):
         self.assertEqual(3, self.game_map.width, "Should get correct cols")
 
     def test_get_set(self):
@@ -27,8 +27,6 @@ class TestClass(unittest.TestCase):
 
     @patch('Map.Map.get_neighbor_count_map')
     def test_update(self, mock_function):
-        # 涉及到地图数据更改，新建一个临时map
-        temp_map = Map([[1, 0, 1], [0, 1, 0], [0, 0, 0], [1, 1, 0]])
         expected_value = [[0, 1, 0],
                           [0, 1, 0],
                           [1, 1, 0],
@@ -37,19 +35,17 @@ class TestClass(unittest.TestCase):
                                       [2, 2, 2],
                                       [3, 3, 2],
                                       [1, 1, 1]]
-        temp_map.Update()
+        self.game_map.Update()
         for i in range(4):
             for j in range(3):
-                self.assertEqual(expected_value[i][j], temp_map.get_cell(i, j))
+                self.assertEqual(expected_value[i][j], self.game_map.get_cell(i, j))
 
     def test_get_neighbor_map(self):
-        expected_value = [[3, 5, 3],
-                          [5, 8, 5],
-                          [5, 8, 5],
-                          [3, 5, 3]]
-        # 涉及到地图数据更改，新建一个临时map
-        temp_map = Map([[1] * 3] * 4)
-        neighbor_count_map = temp_map.get_neighbor_count_map()
+        expected_value = [[1, 3, 1],
+                          [2, 2, 2],
+                          [3, 3, 2],
+                          [1, 1, 1]]
+        neighbor_count_map = self.game_map.get_neighbor_count_map()
         for i in range(4):
             for j in range(3):
                 self.assertEqual(expected_value[i][j], neighbor_count_map[i][j], '(%d, %d)' % (i, j))
