29c2a2492e91583fefd409eedf2604900b2d5b8f
晚霞与你 <201283610@qq.com>
2023-06-05 01:44:29 +0000

    删除文件 GameOfLife.py

--- GameOfLife.py
+++
@@ -1,7 +0,0 @@
-import random
-from Map import Map
-from UI import UI
-
-game_map = Map([[random.choice([0, 1]) for i in range(30)] for j in range(30)])
-GameUI = UI(game_map, "Game Of Life")
-GameUI.play()
